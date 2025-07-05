24dd23b784f8fbe773c1a622f4e3117f6bc30593
Venkatesan, Rajeshkumar <rajeshcse2020@gmail.com>
2025-07-04 23:42:35 -0500

    add array for demo

---
+++ set/main.py
@@ -0,0 +1,19 @@
+def hello():
+    set_test = {"xyz","apple","banana","kiwi"}
+    set_test.add("orange")
+    for x in set_test:
+        print(x)
+
+
+    # y = input("Enter the value \n")
+    # print("Hello World")
+    # print(f"Hellow world {10*10} {x} {y}")
+    # if float(y) > 0:
+    #     print("posstive number")
+    # else:
+    #     print("negative number")
+    # # while y != 10.0 :
+    # #     print("test")
+    # for x in range(1,5,2):
+    #     print(x)
+hello()
